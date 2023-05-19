813a9c2733169abb85e1d34b0a0fc918264498fe
DeniArif <deniarifwibowo82@gmail.com>
2023-05-19 15:19:01 +0700

    comit

--- 188_singlelinkedlist/188_singlelinkedlist.cpp
+++ 188_singlelinkedlist/188_singlelinkedlist.cpp
@@ -101,3 +101,23 @@ void traverse() {
 		}
 	}
 }
+
+void searchData() {
+	if (listEmpty()) {
+		cout << "List Kosong " << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		int nim;
+		cout << "Masukkan nim: ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "nim: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
